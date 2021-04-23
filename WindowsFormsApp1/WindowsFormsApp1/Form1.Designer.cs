a5aedf0cc4b08b83b73cda3d82573cf8f1d655a9
thegomezmc <71182551+thegomezmc@users.noreply.github.com>
2021-04-23 14:11:49 +0300

    Add files via upload
    I got some of the code here (https://github.com/swharden/Csharp-Data-Visualization) and changed it according to my needs.
    Thx @swharden

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(46, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 23);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 84);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(28, 20);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(12, 49);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(28, 20);
+            this.numericUpDown2.TabIndex = 11;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(28, 20);
+            this.numericUpDown3.TabIndex = 15;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 126);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Ses Uyari";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+using NAudio.Wave; // installed with nuget
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer sound = new SoundPlayer("Metal Gear Alert! Sound Effect.wav");
+        private static double audioValueMax = 0;
+        private static double audioValueLast = 0;
+        private static int RATE = 44100;
+        private static int BUFFER_SAMPLES = 1024;
+        private static double lvl1=0;
+        private static double lvl2=0;
+        private static double lvl3=0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            var waveIn = new WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1);
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.BufferMilliseconds = (int)((double)BUFFER_SAMPLES / (double)RATE * 1000.0);
+            waveIn.StartRecording();
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs args)
+        {
+            
+            float max = 0;
+
+            // interpret as 16 bit audio
+            for (int index = 0; index < args.BytesRecorded; index += 2)
+            {
+                short sample = (short)((args.Buffer[index + 1] << 8) |
+                                        args.Buffer[index + 0]);
+                var sample32 = sample / 32768f; // to floating point
+                if (sample32 < 0) sample32 = -sample32; // absolute value 
+                if (sample32 > max) max = sample32; // is this the max value?
+            }
+
+            // calculate what fraction this peak is of previous peaks
+            if (max > audioValueMax)
+            {
+                audioValueMax = (double) max;
+            }
+            audioValueLast = max;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                 
+            
+            if(audioValueLast<=lvl1)
+            {
+                button1.BackColor =Color.Green;
+                button2.BackColor = Color.White;
+                button3.BackColor = Color.White;
+            }
+            if(audioValueLast>=lvl1 && audioValueLast<=lvl2)
+            {
+                button1.BackColor = Color.DarkGreen;
+                button2.BackColor = Color.DarkGreen;
+                button3.BackColor = Color.White;   
+            }
+            if(audioValueLast>=lvl2)
+            {
+                button1.BackColor = Color.Red;
+                button2.BackColor = Color.Red;
+                button3.BackColor = Color.Red;
+            }
+            if (audioValueLast >= lvl3)
+            { 
+                sound.PlaySync(); 
+            }
+         
+            lvl1 = Convert.ToDouble(numericUpDown1.Value) /10 ;
+            lvl2 = Convert.ToDouble(numericUpDown2.Value) /10 ;
+            lvl3 = Convert.ToDouble(numericUpDown3.Value) /10;
+
+            if (numericUpDown1.Value==10 || numericUpDown2.Value<=numericUpDown1.Value)
+            {
+                numericUpDown1.Value = numericUpDown2.Value-1;
+            }
+            if (numericUpDown2.Value == 10 || numericUpDown3.Value <= numericUpDown2.Value)
+            {
+                numericUpDown2.Value = numericUpDown3.Value-1;
+            }
+            if (numericUpDown3.Value == 10)
+            {
+                numericUpDown3.Value = 9;
+            }
+
+        }        
+    }
+}
